f859f2842a6fd6760303bb9f54cd9f2fcd6ed20b
ruofeng.chen <ruofeng.chen@shopee.com>
2023-07-28 14:30:58 +0800

    chore: using new lib to inject css

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
-import cssExtractPlugin from './build/cssExtractPlugin';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 const fileName = 'vue-ellipsis-3';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), cssExtractPlugin()],
+  plugins: [
+    vue(),
+    libInjectCss(),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, './src/index.ts'),
